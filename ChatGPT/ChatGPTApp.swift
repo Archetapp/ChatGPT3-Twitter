4100be3049013202d4776e9fa674a7f25f20be93
Jared Davidson (Archetapp) <archetapp@gmail.com>
2022-12-07 12:18:37 +0000

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Jared Davidson on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
